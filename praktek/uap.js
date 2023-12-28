615bfe877730a922c4378c19f77ac004a1fe2f2b
Dijee <127408012+Dimasjp13@users.noreply.github.com>
2023-12-28 10:05:24 +0700

    Add files via upload

---
+++ praktek/uap.js
@@ -0,0 +1,10 @@
+const Barang = [
+  { Kode: "001", Nama_Barang: "A", Harga: 10000 },
+  { Kode: "002", Nama_Barang: "B", Harga: 20000 },
+  { Kode: "003", Nama_Barang: "C", Harga: 30000 },
+];
+
+function tampil_data() {
+  const barangtable = document.getElementById("barangtable");
+  Barang.forEach((barang, index) => {});
+}
